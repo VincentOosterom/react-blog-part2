052113543d143a7942b9464ab36e1694bddb5930
VincentOosterom <vincent@oosteromstudio.nl>
2025-10-19 18:54:41 +0200

    Opdracht 1.4 af

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import React from "react";
 function App() {
     const [success, setSuccess] = React.useState("");
     const [error, setError] = React.useState("");
+    const [deleteSuccess, setDeleteSuccess] = React.useState(false);
 
     async function getData() {
         try {
@@ -60,13 +61,28 @@ function App() {
         }
     }
 
+    async function deletePost() {
+        try {
+            const response = await axios.delete('https://novi-backend-api-wgsgz.ondigitalocean.app/api/blogposts/21', {
+                headers: {
+                    'novi-education-project-id': "d6200c4d-2a0a-435d-aba6-171c6a7296e"
+                }
+            })
+            setDeleteSuccess("Blogpost is succesvol verwijderd")
+        } catch (e) {
+            console.log('Fout bij het verwijderen van de blog', e)
+        }
+    }
+
     return (
         <>
             <div className="button-section">
                 <button type="button" onClick={getData}>Haal blogs op</button>
                 <button type="button" onClick={getPost}>Haal post 6 op</button>
                 <button type="button" onClick={addPost}>Plaats blogs post</button>
+                <button type="button" onClick={deletePost}>Verwijder blog</button>
                 {success ? <p>Successfully added.</p> : <p>{error}</p>}
+                {deleteSuccess ? <p>Post is succesvol verwijderd</p> : ""}
             </div>
         </>
     )
