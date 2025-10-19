77dcb4c31aec290fbce1c4c58a0d2a646aeb361a
VincentOosterom <vincent@oosteromstudio.nl>
2025-10-19 18:43:11 +0200

    opdracht 1.3 af

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import './App.css'
 import axios from 'axios'
-import {useState} from "react";
+import React from "react";
 
 function App() {
+    const [success, setSuccess] = React.useState("");
+    const [error, setError] = React.useState("");
 
     async function getData() {
         try {
-            const response = await axios.get('https://novi-backend-api-wgsgz.ondigitalocean.app/api/blogposts', {
-                headers: {
-                    'novi-education-project-id': "d6200c4d-2a0a-435d-aba6-6171c6a7296e"
-                }
-            })
+            const response = await axios.get('https://novi-backend-api-wgsgz.ondigitalocean.app/api/blogposts',
+                {
+                    headers: {
+                        'novi-education-project-id': "d6200c4d-2a0a-435d-aba6-6171c6a7296e"
+                    }
+                })
             console.log(response.data)
         } catch (e) {
             console.log('Fout bij het ophalen van de blogs')
@@ -23,10 +26,37 @@ function App() {
                 headers: {
                     'novi-education-project-id': "d6200c4d-2a0a-435d-aba6-6171c6a7296e"
                 }
+
             })
             console.log(response)
         } catch (e) {
             console.log('Fout bij het ophalen van de blogs')
+        }
+    }
+
+    async function addPost() {
+        try {
+            const response = await axios.post('https://novi-backend-api-wgsgz.ondigitalocean.app/api/blogposts',
+                {
+                    "title": "Wat gebruiker heeft ingevuld",
+                    "subtitle": "Wat gebruiker heeft ingevuld",
+                    "content": "Wat gebruiker heeft ingevuld, in dit geval minder dan 100 woorden",
+                    "author": "Voornaam achternaam",
+                    "created": "2023-09-21T09:30:00Z",
+                    "readTime": 1,
+                    "comments": 0,
+                    "shares": 0
+                },
+                {
+                    headers: {
+                        'novi-education-project-id': "d6200c4d-2a0a-435d-aba6-6171c6a7296e"
+                    },
+                })
+            setSuccess("true")
+            console.log(response.data)
+        } catch (e) {
+            console.log('Fout bij het plaatsten van de blog', e)
+            setError("Het is niet gelukt om jouw blog te plaatsen")
         }
     }
 
@@ -35,6 +65,8 @@ function App() {
             <div className="button-section">
                 <button type="button" onClick={getData}>Haal blogs op</button>
                 <button type="button" onClick={getPost}>Haal post 6 op</button>
+                <button type="button" onClick={addPost}>Plaats blogs post</button>
+                {success ? <p>Successfully added.</p> : <p>{error}</p>}
             </div>
         </>
     )
